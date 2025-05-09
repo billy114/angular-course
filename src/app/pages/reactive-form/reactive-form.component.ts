2a1f448fcce2204811692e2e9a9755ce5b56346e
Billel <billel.abbes@hotmail.com>
2025-05-09 12:31:26 +0200

    form group

--- src/app/pages/reactive-form/reactive-form.component.ts
+++ src/app/pages/reactive-form/reactive-form.component.ts
@@ -1,31 +1,61 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgClass
   ],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.scss'
 })
 export class ReactiveFormComponent implements OnInit {
 
-  nameFormControl : FormControl<string | null> = new FormControl ('', [
-    Validators.required, Validators.minLength(6)
-    ]);
-
-  password : FormControl<string | null> = new FormControl ('', [
-    Validators.pattern("^[a-z0-9]*$"),
-  ]);
-  confPssword : FormControl<string | null> = new FormControl ('', []);
-
+  submitted = false;
+  signForm : FormGroup = new FormGroup({
+    name : new FormControl ('', [Validators.required]),
+    email : new FormControl ('', [Validators.required, Validators.email]),
+    password: new FormControl ('', []),
+    confPssword : new FormControl ('', []),
+    languages : new FormArray<FormControl<string | null>>(
+      [
+        new FormControl ('', [Validators.required]),
+      ]
+    )
+  })
 
   ngOnInit(): void {
-    console.log(this.nameFormControl.value)
-    // this.nameFormControl.patchValue("Elie !!");
+
   }
 
+  get name() : FormControl {
+    return this.signForm.controls['name'] as FormControl;
+  }
+
+  get email() : FormControl {
+    return this.signForm.controls['email'] as FormControl;
+  }
+
+  get languages() : FormArray<FormControl<string | null>> {
+    return this.signForm.controls['languages'] as FormArray<FormControl<string | null>>;
+  }
+
+  delete(index : number):void {
+    this.languages.removeAt(index);
+  }
+
+  add() : void {
+    this.languages.push(new FormControl ('', [Validators.required]),)
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    console.log(
+      this.signForm.value
+    );
+  }
 
 }
